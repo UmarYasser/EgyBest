c6302ebc41f8207c36574635a735b16cc5d30616
UmarYasser <110424876+UmarYasser@users.noreply.github.com>
2026-01-11 17:14:10 +0200

    Fixed Routes in pagesRouter.js

--- Routes/pageRouter.js
+++ Routes/pageRouter.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const fs = require("fs");
 
-const Login = fs.readFileSync("./EgyBest/../Public/Pages/Login.html");
-const Signup = fs.readFileSync("./EgyBest/../Public/Pages/Signup.html");
-const Theater = fs.readFileSync("./EgyBest/../Public/Pages/Theater.html");
-const Movie = fs.readFileSync("./EgyBest/../Public/Pages/Movie.html");
+const Login = fs.readFileSync("./EgyBest/Public/Pages/Login.html");
+const Signup = fs.readFileSync("./EgyBest/Public/Pages/Signup.html");
+const Theater = fs.readFileSync("./EgyBest/Public/Pages/Theater.html");
+const Movie = fs.readFileSync("./EgyBest/Public/Pages/Movie.html");
 
 
-const notFound = fs.readFileSync("./EgyBest/../Public/Pages/404.html");
-const resetPassword=  fs.readFileSync("./EgyBest/../Public/Pages/resetPassword.html");
+const notFound = fs.readFileSync("./EgyBest/Public/Pages/404.html");
+const resetPassword=  fs.readFileSync("./EgyBest/Public/Pages/resetPassword.html");
 
 router.get("/(Login(.html)?)?", (req, res) => {
   res.setHeader("Content-Type",'text/html')
